34cd16c3ebeda9f0691f49d28eb81236f0d0e9e3
Tudor Goia <Tudor Goia>
2016-11-12 00:41:06 +0200

    very basic component

--- app.tsx
+++ app.tsx
@@ -2,24 +2,39 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
-navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: false
-    }).then((videoStream) => {
-        let camera: HTMLImageElement =  document.getElementById('camera') as HTMLImageElement;
-        camera.src = URL.createObjectURL(videoStream);
-    });
+// navigator.mediaDevices.getUserMedia({
+//         video: true,
+//         audio: false
+//     }).then((videoStream) => {
+//         let camera: HTMLImageElement =  document.getElementById('camera') as HTMLImageElement;
+//         camera.src = URL.createObjectURL(videoStream);
+//     });
 
-window.onload = function() {
+// window.onload = function() {
 
-    fs.readFile('tsconfig.json', {encoding: 'utf8'}, (err: NodeJS.ErrnoException, buffer: string) => {
+//     fs.readFile('tsconfig.json', {encoding: 'utf8'}, (err: NodeJS.ErrnoException, buffer: string) => {
 
-        let fileContent: JSON = JSON.parse(buffer);
-        ReactDOM.render(
-            <pre>{JSON.stringify(fileContent, null, 4)}</pre>,
-            document.getElementById('hello')
+//         let fileContent: JSON = JSON.parse(buffer);
+//         ReactDOM.render(
+//             <pre>{JSON.stringify(fileContent, null, 4)}</pre>,
+//             document.getElementById('app')
+//         );
+//     });
+// }
+
+interface LayoutProps {};
+interface LayoutState {};
+
+class Layout extends React.Component<LayoutProps, LayoutState> {
+    render() {
+        return (
+            <h1>It works...</h1>
         );
-    });
+    }
 }
+
+const appElement = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, appElement);
