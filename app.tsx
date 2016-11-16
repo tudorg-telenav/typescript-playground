c441b588819c5479d5f0fbf231c78a0ec21108df
tudorg-telenav <tudor.goia@telenav.com>
2016-11-17 01:32:24 +0200

    meet react-router

--- app.tsx
+++ app.tsx
@@ -1,22 +1,39 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import {
+    Router,
+    Route,
+    Redirect,
+    Link,
+    browserHistory
+} from 'react-router';
+
 import Layout from "./pane_components/Layout"
 
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
-// navigator.mediaDevices.getUserMedia({
-//         video: true,
-//         audio: false
-//     }).then((videoStream) => {
-//         let camera: HTMLImageElement =  document.getElementById('camera') as HTMLImageElement;
-//         camera.src = URL.createObjectURL(videoStream);
-//     });
+// fs.readFile('tsconfig.json', {encoding: 'utf8'}, (err: NodeJS.ErrnoException, buffer: string) => {
 
-fs.readFile('tsconfig.json', {encoding: 'utf8'}, (err: NodeJS.ErrnoException, buffer: string) => {
-
-    let fileContent: JSON = JSON.parse(buffer);
+//     let fileContent: JSON = JSON.parse(buffer);
     
-    const appElement: Element = document.getElementById('app');
-    ReactDOM.render(<Layout fileContent={JSON.stringify(fileContent, null, 4)}/>, appElement);
-});
+//     const appElement: Element = document.getElementById('app');
+//     // ReactDOM.render(<Layout fileContent={JSON.stringify(fileContent, null, 4)}/>, appElement);
+//     ReactDOM.render(
+//         <Router history={browserHistory}>
+//             <Route path="*" component={Layout}>
+//             </Route>
+//         </Router>,
+//         appElement
+//     );
+// });
+
+const appElement: Element = document.getElementById('app');
+ReactDOM.render(
+    <Router history={browserHistory}>
+        <Route path="home" component={Layout}>
+        </Route>
+        <Redirect path="*" to="home" />
+    </Router>,
+    appElement
+);
